7303ecca0788a0173a190e78a4588f1d5d245321
cindy21td <m.cindy.prasetio@gmail.com>
2015-10-18 12:41:47 -0700

    Leap Ovr

---
+++ Assets/MovementController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class MovementController : MonoBehaviour {
+    Controller controller;
+
+	public Camera camera = null;
+	public float _moveSpeed;
+	public GameObject leapMotionOVRController = null;
+	
+	void Start () {
+        controller = new Controller();
+        controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+		controller.Config.SetFloat ("Gesture.Swipe.MinLength", 100.0f);
+		controller.Config.SetFloat ("Gesture.Swipe.MinVelocity", 50f);
+		controller.Config.Save ();
+    }
+	
+	
+	void Update () {
+        Frame frame = controller.Frame();
+        GestureList gestures = frame.Gestures();
+        for (int i = 0; i < gestures.Count; i++)
+        {
+            Gesture gesture = gestures[i];
+            if (gesture.Type == SwipeGesture.ClassType())
+            {
+				leapMotionOVRController.transform.localPosition += camera.transform.forward * _moveSpeed * Time.deltaTime;
+                Debug.Log("yoo");
+            }
+        }
+	}
+}
